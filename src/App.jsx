748d346556e6741704c85ca6b5a0d5c8d43ef8eb
saidur20087 <saidur.20087@gmail.com>
2026-03-29 21:30:06 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='font-bold text-green-500 text-center text-4xl'>My Assignment 06</h1>
+    <h1 className='font-bold text-green-500 text-center text-4xl'>Welcome my page</h1>
+
+      
+    </>
+  )
+}
+
+export default App
